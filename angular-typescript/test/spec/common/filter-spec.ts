afb64dfc228ec68014eb1396bd827f47eefaf153
Flemming Bregnvig <flemming@bregnvig.dk>
2016-01-06 11:10:32 +0100

    Added module

--- angular-typescript/test/spec/common/filter-spec.ts
+++ angular-typescript/test/spec/common/filter-spec.ts
@@ -1,45 +1,49 @@
-import IHumanizeDistanceFilter = playgrounds.common.filters.IHumanizeDistanceFilter;
-import IDefaultDescriptionFilter = playgrounds.common.filters.IDefaultDescriptionFilter;
-'use strict';
+module playgrounds.test.spec.common {
 
-describe('Filters', () => {
+  import IHumanizeDistanceFilter = playgrounds.common.filters.IHumanizeDistanceFilter;
+  import IDefaultDescriptionFilter = playgrounds.common.filters.IDefaultDescriptionFilter;
 
-  beforeEach(angular.mock.module('playgrounds'));
+  'use strict';
 
-  describe('Humanize', () => {
-    let humanizer: IHumanizeDistanceFilter;
-    beforeEach(inject((_$filter_: angular.IFilterService) => {
-      humanizer = _$filter_<IHumanizeDistanceFilter>('humanizeDistance');
-    }));
+  describe('Filters', () => {
 
-    it('Should be to far to walk when the distance is 2km or more', () => {
-      expect('Ikke til fods!').toEqual(humanizer(2000));
-      expect('Ikke til fods!').toEqual(humanizer(20000));
+    beforeEach(angular.mock.module('playgrounds'));
+
+    describe('Humanize', () => {
+      let humanizer: IHumanizeDistanceFilter;
+      beforeEach(inject((_$filter_: angular.IFilterService) => {
+        humanizer = _$filter_<IHumanizeDistanceFilter>('humanizeDistance');
+      }));
+
+      it('Should be to far to walk when the distance is 2km or more', () => {
+        expect('Ikke til fods!').toEqual(humanizer(2000));
+        expect('Ikke til fods!').toEqual(humanizer(20000));
+      });
+      it('Should be to ok to walk when the distance is less than 2km', () => {
+        expect('Et stykke vej').toEqual(humanizer(1999));
+      });
+      it('Should return the distance in meters, when it is less than 750 meters', () => {
+        expect('749 m').toEqual(humanizer(749));
+        expect('51 m').toEqual(humanizer(51));
+      });
+      it('Should return unknown, if the distance is unknown', () => {
+        expect('Ukendt').toEqual(humanizer(undefined));
+      });
     });
-    it('Should be to ok to walk when the distance is less than 2km', () => {
-      expect('Et stykke vej').toEqual(humanizer(1999));
-    });
-    it('Should return the distance in meters, when it is less than 750 meters', () => {
-      expect('749 m').toEqual(humanizer(749));
-      expect('51 m').toEqual(humanizer(51));
-    });
-    it('Should return unknown, if the distance is unknown', () => {
-      expect('Ukendt').toEqual(humanizer(undefined));
+
+    describe('Default description', () => {
+      let defaultDescription: IDefaultDescriptionFilter;
+      beforeEach(inject((_$filter_: angular.IFilterService) => {
+        defaultDescription = _$filter_<IDefaultDescriptionFilter>('defaultDescription');
+      }));
+      it('should return the given value, when the value is not undefined', () => {
+        expect('Hello world').toEqual(defaultDescription('Hello world'));
+      });
+      it('should return a default description, when the value is undefined', () => {
+        expect('Ingen beskrivelse').toEqual(defaultDescription(undefined));
+        expect('Ingen beskrivelse').toEqual(defaultDescription(null));
+        expect('Ingen beskrivelse').toEqual(defaultDescription(0));
+      });
     });
   });
-
-  describe('Default description', () => {
-    let defaultDescription: IDefaultDescriptionFilter;
-    beforeEach(inject((_$filter_: angular.IFilterService) => {
-      defaultDescription = _$filter_<IDefaultDescriptionFilter>('defaultDescription');
-    }));
-    it('should return the given value, when the value is not undefined', () => {
-      expect('Hello world').toEqual(defaultDescription('Hello world'));
-    });
-    it('should return a default description, when the value is undefined', () => {
-      expect('Ingen beskrivelse').toEqual(defaultDescription(undefined));
-      expect('Ingen beskrivelse').toEqual(defaultDescription(null));
-      expect('Ingen beskrivelse').toEqual(defaultDescription(0));
-    });
-  });
-});
+}
